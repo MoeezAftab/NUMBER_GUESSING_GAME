a7f6efef8585fbc6b407faeea5c5e68867308922
MoeezAftab <163597623+MoeezAftab@users.noreply.github.com>
2024-04-30 00:57:03 +0500

    i have created guessing number game

---
+++ Number_Guessing_Game.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+
+import inquirer from "inquirer"
+
+let roundNumber: number = Math.floor((Math.random() * 4 ) + 1);
+
+let userInput = await inquirer.prompt({
+    type: 'number',
+    name: 'guessedNumber',
+    message: 'guess a number between 1 -4'
+})
+
+if(userInput.guessedNumber === roundNumber){
+    console.log(`Congratulation! You Guessed Correctly!`);
+
+}else{
+    console.log(`Sorry! Try next time. Correct Ans is: ${roundNumber}`);
+}
